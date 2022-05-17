110ecde6515a38412e6d882d659a5d7f49f01438
Robert Bangiyev <rbangiyev123@gmail.com>
2022-05-16 20:52:46 -0400

    Trying to fix error

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.listen(5000);
+app.listen(process.env.PORT || 5000);
 
 app.get('/', (req, res) => {
     res.redirect('/api');
